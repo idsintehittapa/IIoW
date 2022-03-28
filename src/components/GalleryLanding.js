92ddc7db27f077c4bb74fe1370ff3e3887a8382b
idsintehittapa <jblom15@gmail.com>
2022-03-28 16:35:18 +0200

    adding gallery hover on landing page

---
+++ src/components/GalleryLanding.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import bell from '../assets/h1380-0cce1.jpg';
+import woolf from '../assets/087dacdb43f5bf5cca958d9100145272.jpg';
+import woolfOverlay from '../assets/Woolf_Overlay.png';
+import bellOverlay from '../assets/bellOverlay.png';
+
+import '../components/galleryLanding.css';
+
+export const GalleryLanding = () => {
+  return (
+    <section className="gallery__wrapping">
+      <div className="gallery__images">
+        <div className="woolf__wrapper">
+          <img className="landing__woolf" src={woolf} alt="virginia woolf" />
+          <img
+            alt="virginia woolf overlay"
+            className="overlay"
+            src={woolfOverlay}
+          />
+        </div>
+        <div className="woolf__wrapper">
+          <img className="landing__bell" src={bell} alt="bell hooks" />
+          <img
+            alt="bell hooks overlay"
+            className="overlay__bell"
+            src={bellOverlay}
+          />
+        </div>
+      </div>
+    </section>
+    // <section className="gallery__wrapping">
+    //   <div className="gallery__images">
+    //     <div className="woolf__wrapper">
+    //       <img className="landing__woolf" src={woolf} alt="##" />
+    //       <img alt="##" className="overlay" src={woolfOverlay} />
+    //     </div>
+    //     <div className="woolf2__wrapper">
+    //       <img className="landing__woolf2" src={bell} alt="##" />
+    // <img alt="##" className="overlay" src={woolfOverlay} />
+    // </div>
+    //   </div>
+    // </section>
+  );
+};
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Hero } from '../components/Hero';
-import bell from '../assets/h1380-0cce1.jpg';
-import woolf from '../assets/087dacdb43f5bf5cca958d9100145272.jpg';
 import './landingPage.css';
 import { LandingPageImage } from '../Animations/LandingPageImage';
 import { EncyclopediaLanding } from '../components/EncyclopediaLanding';
+import { GalleryLanding } from '../components/GalleryLanding';
 
 export const LandingPage = () => {
   return (
@@ -21,10 +20,8 @@ export const LandingPage = () => {
           amet, consectetur adipiscing elit.
         </p>
       </div>
-      <div className="landing__images">
-        <img className="landing_woolf" src={woolf} alt="##" />
-        <img className="landing_bell" src={bell} alt="##" />
-      </div>
+      <GalleryLanding />
+
       <div className="landing__content-two">
         <p className="landing__p">
           <span className="landing__p-span">(xxxx)</span> Lorem ipsum dolor sit
