31255f81cbf97d535ef6aca9f3e829004282b7b5
idsintehittapa <jblom15@gmail.com>
2022-03-29 16:44:21 +0200

    adding draft of content

---
+++ src/pages/Encyclopedia.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './encyclopedia.css';
+
+export const Encyclopedia = () => {
+  return (
+    <section className="encyclopedia__wrapper">
+      <h1 className="encyclopedia__title">Encyclopedia</h1>
+      <div className="hanna"></div>
+    </section>
+  );
+};
