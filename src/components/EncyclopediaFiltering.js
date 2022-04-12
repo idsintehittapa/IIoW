ebb1ba66fc6333d821f633ecafff4599ec425503
idsintehittapa <jblom15@gmail.com>
2022-04-12 16:33:27 +0200

    adding query and search filters

--- src/components/EncyclopediaFiltering.js
+++ src/components/EncyclopediaFiltering.js
@@ -1,60 +1,90 @@
-import React from 'react';
-import { gql, useQuery } from '@apollo/client';
-// import { useAuthenticated } from '@nhost/react';
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_THINKER } from '../queries/queries';
 
 import '../pages/encyclopedia.css';
 
-// const GET_BF = gql`
-//   thinker_person {
-//     country
-//     name
-//     person_fields {
-//       field {
-//       name
-//     }
-//   }
-//   person_categories {
-//     category {
-//       name
-//     }
-//   }
-// }
-// }
-// `;
-
-const GET_THINKER = gql`
-  query Thinker {
-    thinker_person {
-      country
-      name
-      id
-    }
-  }
-`;
-
-export const EncyclopediaFiltering = (category) => {
-  // const isAuthenticated = useAuthenticated();
-  const { data, loading, error, refetch } = useQuery(GET_THINKER, {
-    variables: { category }
-  });
+export const EncyclopediaFiltering = () => {
+  const { data, loading, error } = useQuery(GET_THINKER);
+  const [filteredData, setFilteredData] = useState(data);
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
+
+  const filter = () => {
+    const fd = data.thinker_person.filter((thinker) => {
+      let found = false;
+      thinker.person_categories.map((category) => {
+        found = found || category.category.name === 'philosopher';
+        return found;
+      });
+      return found;
+    });
+    setFilteredData(fd);
+  };
+
+  const filterBF = () => {
+    const fd = data.thinker_person.filter((thinker) => {
+      let found = false;
+      thinker.person_fields.map((field) => {
+        found = found || field.field.name === 'black feminism';
+        return found;
+      });
+      return found;
+    });
+    setFilteredData(fd);
+  };
+
+  const filterLove = () => {
+    const fd = data.thinker_person.filter((thinker) => {
+      let found = false;
+      thinker.person_fields.map((field) => {
+        found = found || field.field.name === 'love';
+        return found;
+      });
+      return found;
+    });
+    setFilteredData(fd);
+  };
+
+  const filterMother = () => {
+    const fd = data.thinker_person.filter((thinker) => {
+      let found = false;
+      thinker.person_fields.map((field) => {
+        found = found || field.field.name === 'motherhood';
+        return found;
+      });
+      return found;
+    });
+    setFilteredData(fd);
+  };
 
   return (
     <>
       <div className="encyclopedia__searchbar">
         <div className="encyclopedia__searchbar-filter">
-          <button className="encyclopedia__button">black feminism</button>
-          <button className="encyclopedia__button">love </button>
-          <button className="encyclopedia__button">motherhood</button>
+          <button className="encyclopedia__button" onClick={() => filter()}>
+            philosopher
+          </button>
+          <button className="encyclopedia__button" onClick={() => filterBF()}>
+            black feminism{' '}
+          </button>
+          <button
+            className="encyclopedia__button"
+            onClick={() => filterMother()}
+          >
+            motherhood
+          </button>
+          <button className="encyclopedia__button" onClick={() => filterLove()}>
+            love
+          </button>
         </div>
         <div className="encyclopedia__searchbar-search">
           <p>SEARCH</p>
         </div>
       </div>
       <div className="encyclopedia__query">
-        {data?.thinker_person.map((thinker) => (
+        {filteredData?.map((thinker) => (
           <div key={thinker.id}>
             <p>{thinker.name}</p>
           </div>
