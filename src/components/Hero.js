65712edbc0c7edbdb5ad8c0f0b1f25c853fc0ef0
idsintehittapa <jblom15@gmail.com>
2022-04-23 16:09:10 +0200

    adding changed hero layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,89 @@ import React from 'react';
 import './hero.css';
 
 export const Hero = () => {
+  const scrollGalleryImages2 = [
+    {
+      src: require('../assets/GettyImages_514704088_header.jpg'),
+      alt: 'toni morrison'
+    },
+    {
+      src: require('../assets/gettyimages-694880169-copy.jpg'),
+      alt: 'audry lorde'
+    },
+    {
+      src: require('../assets/Sylvia-Plath.jpg'),
+      alt: 'sylvia plath'
+    },
+    {
+      src: require('../assets/Liv_Stromquist.png'),
+      alt: 'liv stromquist'
+    },
+    {
+      src: require('../assets/simone-de-beauvoir.png'),
+      alt: 'simone de beauvoir'
+    },
+    {
+      src: require('../assets/Butler.jpg'),
+      alt: 'judith butler'
+    },
+    {
+      src: require('../assets/bettyF.png'),
+      alt: 'betty friedan'
+    },
+    {
+      src: require('../assets/crf.png'),
+      alt: 'caroline ringskog ferrada-noli'
+    },
+    {
+      src: require('../assets/sontage.jpg'),
+      alt: 'susan sontage'
+    },
+    {
+      src: require('../assets/GettyImages_514704088_header.jpg'),
+      alt: 'toni morrison'
+    },
+    {
+      src: require('../assets/gettyimages-694880169-copy.jpg'),
+      alt: 'audry lorde'
+    },
+    {
+      src: require('../assets/Sylvia-Plath.jpg'),
+      alt: 'sylvia plath'
+    },
+    {
+      src: require('../assets/Liv_Stromquist.png'),
+      alt: 'liv stromquist'
+    },
+    {
+      src: require('../assets/simone-de-beauvoir.png'),
+      alt: 'simone de beauvoir'
+    },
+    {
+      src: require('../assets/Butler.jpg'),
+      alt: 'judith butler'
+    },
+    {
+      src: require('../assets/bettyF.png'),
+      alt: 'betty friedan'
+    },
+    {
+      src: require('../assets/crf.png'),
+      alt: 'caroline ringskog ferrada-noli'
+    },
+    {
+      src: require('../assets/sontage.jpg'),
+      alt: 'susan sontage'
+    }
+  ];
+
   return (
     <>
       <header className="hero__image">
+        {scrollGalleryImages2.map((image, index) => (
+          <div className="hero__gallery" key={index}>
+            <img src={image.src} alt={image.alt} />
+          </div>
+        ))}
         <h1 className="hero__title">Intellectual Influence of Women</h1>
       </header>
       <div className="color"></div>
